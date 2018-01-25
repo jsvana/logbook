9e5e4d10941f389c59ff1924b2d6a3422df8f684
Jay Vana <jsvana@mtu.edu>
2018-01-24 20:15:35 -0800

    Multiple graphs

--- logbook/server.py
+++ logbook/server.py
@@ -5,6 +5,7 @@ from flask import (
     send_from_directory,
     Flask,
 )
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -29,19 +30,31 @@ def send_js(path):
     return send_from_directory('static/js', path)
 
 
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+
+def return_json(text, status):
+    return app.response_class(
+        response=text,
+        status=status,
+        mimetype='application/json',
+    )
+
+
 @app.route('/aircraft')
 def aircraft():
     engine = create_engine('sqlite:///logbook.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    return app.response_class(
-        response=json.dumps(
+    return return_json(
+        json.dumps(
             list(session.query(Aircraft).all()),
             cls=AlchemyEncoder,
         ),
-        status=200,
-        mimetype='application/json',
+        200,
     )
 
 
@@ -52,14 +65,58 @@ def flights():
     session = Session()
     print([f.pic for f in session.query(Flight).all()])
 
-    return app.response_class(
-        response=json.dumps(
+    return return_json(
+        json.dumps(
             list(session.query(Flight).order_by(Flight.date).all()),
             cls=AlchemyEncoder,
         ),
-        status=200,
-        mimetype='application/json',
+        200,
     )
+
+
+@app.route('/airport/<path:path>')
+def airport(path):
+    req = requests.post(
+        'https://openflights.org/php/apsearch.php',
+        {
+            'name': '',
+            'iata': '',
+            'icao': path.upper(),
+            'city': '',
+            'country': 'United States',
+            'code': 'US',
+            'x': '',
+            'y': '',
+            'elevation': '',
+            'timezone': '',
+            'dst': 'U',
+            'db': 'airports',
+            'iatafilter': 'true',
+            'apid': '',
+            'action': 'SEARCH',
+            'offset': '0',
+        },
+    )
+    if req.status_code != 200:
+        return return_json(
+            json.dumps({'error': 'Unable to process request'}),
+            500,
+        )
+
+    apt = None
+    for airport in req.json()['airports']:
+        if airport['icao'] == path.upper():
+            apt = airport
+            break
+    else:
+        return return_json(
+            json.dumps(
+                {'error': 'Unable to find airport {}'.format(path.upper())},
+            ),
+            404,
+        )
+
+    return return_json(json.dumps(apt), 200)
 
 
 def run(port):
--- logbook/static/js/visualize.js
+++ logbook/static/js/visualize.js
@@ -1,4 +1,4 @@
-var makeRequest = function(path, onSuccess, onError) {
+var getRequest = function(path, onSuccess, onError) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', path);
   xhr.onload = function() {
@@ -14,33 +14,149 @@ var makeRequest = function(path, onSuccess, onError) {
 };
 
 var redraw = function() {
-  var axis = document.querySelector('select[name="axis"]').value;
+  if (!window.flights) {
+    return;
+  }
+
+  var selected = document.querySelectorAll('select[name="axis"] option:checked');
+  var axes = Array.from(selected).map((el) => el.value);
   var cumulative = document.querySelector('#cumulative_true').checked;
 
-  makeRequest(
-    '/flights',
-    function(data) {
-      var graphData = {
-        x: [],
-        y: [],
-        type: 'scatter',
-      };
-      console.log(cumulative);
+  var lines = [];
+  for (var a in axes) {
+    var axis = axes[a];
+    var line = {
+      x: [],
+      y: [],
+      type: 'scatter',
+      name: axis,
+    };
 
-      var total = 0;
-      for (var f in data) {
-        graphData.x.push(data[f].date);
-        var value = data[f][axis];
-        if (cumulative) {
-          total += value;
-          value = total;
-        }
-        graphData.y.push(value);
+    var total = 0;
+    for (var f in window.flights) {
+      line.x.push(window.flights[f].date);
+      var value = window.flights[f][axis];
+      if (cumulative) {
+        total += value;
+        value = total;
       }
+      line.y.push(value);
+    }
+    lines.push(line);
+  }
 
-      Plotly.newPlot('graph', [graphData]);
+  var layout = {
+    title: 'Logbook Data',
+    xaxis: {
+      title: 'Date',
+      showgrid: false,
+      zeroline: false,
+    },
+    yaxis: {
+      title: 'Hours',
+      showline: false,
+    },
+  };
+
+  Plotly.newPlot('graph', lines, layout);
+};
+
+getRequest(
+  '/flights',
+  function(data) {
+    window.flights = data;
+  },
+);
+
+var getLatLong = function(airport, onSuccess, onFailure) {
+  getRequest(
+    '/airport/' + airport,
+    onSuccess,
+    function(xhr) {
+      onFailure(airport, xhr);
     },
   );
+};
+
+var getAllAirports = function(airports, onSuccess) {
+  var counter = 0;
+  for (var a in airports) {
+    airports[a] = airports[a].toUpperCase();
+  }
+  var uniqueAirports = Array.from(new Set(airports));
+  var failedAirports = [];
+  var airportData = {};
+  console.log(uniqueAirports);
+  for (var i in uniqueAirports) {
+    getLatLong(
+      uniqueAirports[i],
+      function(data) {
+        airportData[data.icao] = data;
+        counter++;
+        if (counter === uniqueAirports.length) {
+          onSuccess(airportData);
+        }
+      },
+      function(airport, xhr) {
+        failedAirports.push(airport);
+        console.log('failed ' + airport);
+        counter++;
+        if (counter === uniqueAirports.length) {
+          onSuccess(airportData);
+        }
+      },
+    );
+  }
+};
+
+var mapFlights = function(flights, airports) {
+  var data = [];
+
+  for (var f in flights) {
+    flight = flights[f];
+    var from = airports[flight._from];
+    var to = airports[flight.to];
+
+    if (!from || !to) {
+      console.log('Skipping ' + flight._from + ' to ' + flight.to);
+      continue;
+    }
+
+    var startLat = from.x;
+    var startLong = from.y;
+    var endLat = to.x;
+    var endLong = to.y;
+
+    var result = {
+      type: 'scattergeo',
+      locationmode: 'USA-states',
+      lon: [startLong, endLong],
+      lat: [startLat, endLat],
+      mode: 'lines',
+      line: {
+        width: 1,
+        color: 'red'
+      },
+      opacity: 1,
+    };
+    data.push(result);
+  }
+
+  var layout = {
+    title: 'asdf',
+    showlegend: false,
+    geo:{
+      scope: 'north america',
+      projection: {
+        type: 'azimuthal equal area'
+      },
+      showland: true,
+      landcolor: 'rgb(243,243,243)',
+      countrycolor: 'rgb(204,204,204)'
+    }
+  };
+
+  Plotly.plot('map', data, layout, {showLink: false});
 };
 
 redraw();
